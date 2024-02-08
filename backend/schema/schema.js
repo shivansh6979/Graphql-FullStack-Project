6371312d2b822b1d03277b91b95c2b3ca4b117ad
shivansh6979 <shivansh11g@gmail.com>
2024-02-08 08:30:19 +0530

    Db connection,Project and Client Queries

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,3 +1,5 @@
+const Client = require("../models/Client");
+const Project = require("../models/Project");
 const { clients, projects } = require("../sampleData");
 const {
   GraphQLObjectType,
@@ -18,7 +20,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id == parent.clientId);
+        return Client.findById(parent.id);
       },
     },
   }),
@@ -42,28 +44,29 @@ const RootQuery = new GraphQLObjectType({
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
+
     //client
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
   },
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const colors = require("colors");
+require("dotenv").config();
 const app = express();
 
+// for removing the cors error
 app.use(cors());
+// For having colorful consoles
+colors.enable();
+// Connect to DB
+connectDB();
+
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema: schema,
-    graphiql: process.env.NODE_ENV === 'DEVELOPMENT',
+    graphiql: process.env.NODE_ENV === "DEVELOPMENT",
   })
 );
 
 app.listen(process.env.PORT, () => {
-  console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+  console.log("Running a GraphQL API server at http://localhost:4000/graphql");
 });
